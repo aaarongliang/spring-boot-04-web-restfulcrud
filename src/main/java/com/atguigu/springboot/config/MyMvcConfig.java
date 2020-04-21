70af11dc2a1afbcd06553a421eb9743674ceeefe
partner <mr_liangyu@sina.com>
2020-04-21 16:32:54 +0800

    引入bootstrap的maven依赖

--- src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
+++ src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -1,14 +1,27 @@
 package com.atguigu.springboot.config;
 
+import com.atguigu.springboot.component.MyLocaleResolver;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/wawa").setViewName("success");
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/index.html").setViewName("login");
+        registry.addViewController("index.jsp").setViewName("login");
+        registry.addViewController("login.jsp").setViewName("login");
+        registry.addViewController("login.html").setViewName("login");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
